fbaacf38ad8330a7c0a169bfbf3eba913af926b7
dahhou.ilyas <dahhou.ilyas@gmail.com>
2024-10-05 11:17:52 +0100

    create more endpoint

--- src/main/java/com/example/reactive_dashboard/web/SocieteReactiveController.java
+++ src/main/java/com/example/reactive_dashboard/web/SocieteReactiveController.java
@@ -3,11 +3,10 @@ package com.example.reactive_dashboard.web;
 import com.example.reactive_dashboard.dao.SocieteRopisotory;
 import com.example.reactive_dashboard.entities.Societe;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
 
 @RestController
 public class SocieteReactiveController {
@@ -25,4 +24,20 @@ public class SocieteReactiveController {
         return societeRopisotory.findById(id);
     }
 
+    @PostMapping("/societs")
+    public Mono<Societe> save(@RequestBody Societe societe){
+        societe.setId(societe.getName());
+        return societeRopisotory.save(societe);
+    }
+
+    @DeleteMapping(value = "/societs/{id}")
+    public Mono<Void> delete(@PathVariable String id){
+        return societeRopisotory.deleteById(id);
+    }
+
+    @PutMapping(value = "/societs/{id}")
+    public Mono<Societe> update(@PathVariable String id, @RequestBody Societe societe){
+        societe.setId(id);
+        return societeRopisotory.save(societe);
+    }
 }
